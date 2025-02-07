0a1902cd1ccc2ba5b7c443dbe42d6d02f29726b8
Madalina Gomei <madalinagomei@gmail.com>
2025-02-07 11:50:37 +0100

    ch

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import { App } from 'components/App';
 import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor } from './redux/store';
 
@@ -15,11 +16,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      {/* <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <HashRouter basename="/goit-react-hw-08-phonebook">
         <ChakraProvider theme={myTheme}>
           <App />
         </ChakraProvider>
-      </BrowserRouter> */}
+      </HashRouter>
     </PersistGate>
   </Provider>
   // </React.StrictMode>
