2ca59b24d93f7bdc638642a25f5b10adf67c4d27
Madalina Gomei <madalinagomei@gmail.com>
2025-02-07 11:44:49 +0100

    up

--- src/index.js
+++ src/index.js
@@ -15,11 +15,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      {/* <BrowserRouter basename="/goit-react-hw-08-phonebook">
         <ChakraProvider theme={myTheme}>
           <App />
         </ChakraProvider>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </PersistGate>
   </Provider>
   // </React.StrictMode>
